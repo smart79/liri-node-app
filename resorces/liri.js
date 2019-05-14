d51ce5fcfea5687dcc77d55174080d1f7316d78e
Chris Martinez <smartinez79@protonmail.com>
2019-05-14 07:24:20 -0500

    update to switch method

--- resorces/liri.js
+++ resorces/liri.js
@@ -5,13 +5,43 @@ var keys = require("./keys.js");
 var Spotify = require("node-spotify-api");
 var axios = require("axios");
 var fs = require("fs");
-var moment = require("moment")
+var moment = require("moment");
 // arguments entered by user //
 var appCommand = process.argv[2];
 // console.log("appCommand");
 
 // Slice Method//
-var search = process.argv.slice(3).join(" ");
+var userSearch = process.argv.slice(3).join(" ");
 // console.log("search: "+ search);
 
 // Switch Statement for executing proper command based off user search input.//
+function liriRun(appCommand, userSearch) {
+    
+        switch (appCommand) {
+            
+            case "spotify-this-song":
+                    getSpotify(userSearch);
+                    break;
+            
+            case "movie-this":
+                    getOMDB(userSearch);
+                    break;
+             
+            case "concert-this":
+                    getBandsInTown(userSearch);
+                    break;
+
+            case "do-what-it-says":
+                    getRandom();
+                    break;
+            
+            default:
+                console.log("Try again");
+        }; 
+    }
+
+
+
+
+
+};
